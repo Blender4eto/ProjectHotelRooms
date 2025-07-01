6f3d521adb7f62ea7b22a1c1cbb86d0cf2679deb
Denis Egemen <denka050908@gmail.com>
2025-07-01 16:41:57 +0300

    Added Classes: Data, Room, Constants Corrected Data.cs to Hotel rooms instead of books

---
+++ Data.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ProjectHotelRooms
+{
+    using System.Text.Json;
+    using static Constants;
+    public  class Data
+    {
+        public List<Room> Rooms { get; private set; }
+
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        public Data()
+        {
+            LoadRooms();
+        }
+
+        public void Save()
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            using (writer)
+            {
+                string jsonData = JsonSerializer.Serialize(Rooms);
+                writer.Write(jsonData);
+            }
+        }
+
+        public void LoadRooms()
+        {
+            reader = new StreamReader(filePath);
+            using (reader)
+            {
+                string jsonData = reader.ReadToEnd();
+                Rooms = JsonSerializer.Deserialize<List<Room>>(jsonData)!;
+            }
+            Rooms ??= new List<Room>();
+        }
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHotelRooms
+{
+#nullable disable
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public decimal PricePerNight { get; set; }
+        public bool Occupied { get; set; }
+        public string GuestName { get; set; }
+
+        public Room(int roomNum, string type, int capacity, decimal pricePerNight, bool occupied, string guestName)
+        {
+
+            RoomNumber = roomNum;
+            Type = type;
+            Capacity = capacity;
+            PricePerNight = pricePerNight;
+            Occupied = occupied;
+            GuestName = guestName;
+        }
+
+    }
+}
