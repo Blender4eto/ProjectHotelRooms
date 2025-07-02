a6454660d02955a9cc872c01986f7fb07248b3b0
Mitkocrazy <aklamechka69@gmail.com>
2025-07-02 11:15:10 +0300

    updated the switch

--- Program.cs
+++ Program.cs
@@ -8,32 +8,54 @@
             string choice;
             while ((choice = Console.ReadLine()) != "x")
             {
+                DisplayMenu();
                 switch (choice)
                 {
                     case "1": //Reservation
                         Console.WriteLine("Свободни стаи за резервиране:");
                         ReservateRoom();
-
                         break;
                     case "2": //Exit from room
+                        ExitRoom();
                         break;
                     case "3": //Check avaible rooms and prices
                         DisplayAvaibleRoomUI();
                         break;
                     case "4": //check occupied rooms ant their occupiers
+                        DisplayOccupiedRoomUI();
                         break;
 
                 }
             }
         }
-
-        private static Room DisplayAvaibleRoomUI()
+        private static void DisplayMenu()
         {
-            throw new NotImplementedException();
+            // TODO: Implement application menu
         }
         private static Room ReservateRoom()
         {
+            // TODO: Implement list with avaible rooms and choose number for one of them to reserve
             throw new NotImplementedException();
         }
+        private static Room ExitRoom()
+        {
+            // TODO: Implement list with occupied rooms, enter your name, find the room you want to exit via name
+            throw new NotImplementedException();
+        }
+
+        private static Room DisplayAvaibleRoomUI()
+        {
+            // TODO: Implement lsit with avaible rooms
+            //better use where;
+            throw new NotImplementedException();
+        }
+
+        private static Room DisplayOccupiedRoomUI()
+        {
+            // TODO: Implement lsit with occupied rooms and their owners
+            //better use where;
+            throw new NotImplementedException();
+        }
+
     }
 }
