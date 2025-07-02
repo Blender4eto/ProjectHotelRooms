90fdee9e4060aeeb10fa564a8f53d251cc6065f9
Denis Egemen <denka050908@gmail.com>
2025-07-02 15:45:57 +0300

    added methods to ServeiceUI.cs and directed actions from program.cs to Serveice.cs Not fully complete - to be added/fixed more features

--- Program.cs
+++ Program.cs
@@ -3,61 +3,36 @@
     internal class Program
     {
         private static Data data = new Data();
+        private static ServiceUI serviceUI = new ServiceUI();
         public static void Main(string[] args)
         {
+            serviceUI.DisplayMenu();
             string choice;
             while ((choice = Console.ReadLine()) != "x")
             {
-                DisplayMenu();
                 switch (choice)
                 {
                     case "1": //Reservation
-                        Console.WriteLine("Свободни стаи за резервиране:");
-                        ReservateRoom();
+                        serviceUI.ReservateRoom();
                         data.Save();
+                        serviceUI.DisplayMenu();
                         break;
-                    case "2": //Exit from room
-                        ExitRoom();
+                    case "2": //Leave room
+                        serviceUI.LeaveRoom();
                         data.Save();
+                        serviceUI.DisplayMenu();
                         break;
                     case "3": //Check avaible rooms and prices
-                        DisplayAvaibleRoomUI();
+                        serviceUI.DisplayAvaibleRoomUI();
+                        serviceUI.DisplayMenu();
                         break;
-                    case "4": //check occupied rooms ant their occupiers
-                        DisplayOccupiedRoomUI();
+                    case "4": //Check occupied rooms ant their occupiers
+                        serviceUI.DisplayOccupiedRoomUI();
+                        serviceUI.DisplayMenu();
                         break;
 
                 }
             }
         }
-        private static void DisplayMenu()
-        {
-            // TODO: Implement application menu
-        }
-        private static Room ReservateRoom()
-        {
-            // TODO: Implement list with avaible rooms and choose number for one of them to reserve
-            throw new NotImplementedException();
-        }
-        private static Room ExitRoom()
-        {
-            // TODO: Implement list with occupied rooms, enter your name, find the room you want to exit via name
-            throw new NotImplementedException();
-        }
-
-        private static Room DisplayAvaibleRoomUI()
-        {
-            // TODO: Implement lsit with avaible rooms
-            //better use where;
-            throw new NotImplementedException();
-        }
-
-        private static Room DisplayOccupiedRoomUI()
-        {
-            // TODO: Implement lsit with occupied rooms and their owners
-            //better use where;
-            throw new NotImplementedException();
-        }
-
     }
 }
--- ServiceUI.cs
+++ ServiceUI.cs
@@ -2,12 +2,121 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ProjectHotelRooms
 {
-    internal class ServiceUI
+    public class ServiceUI
     {
+        private static Data data = new Data();
+        public void DisplayMenu()
+        {
+            Console.WriteLine("-----------------Хотел Черноморец-----------------");
+            Console.WriteLine("Списък с предлагани услуги:");
+            Console.WriteLine("1. Резервиране на стая");
+            Console.WriteLine("2. Освобождаване на стая");
+            Console.WriteLine("3. Проверка на наличността и цените на стаите");
+            Console.WriteLine("4. Справка за заетите стаи и техните гости");
+            Console.WriteLine("x. Изход от програмата");
+            Console.WriteLine("--------------------------------------------------");
+            Console.Write("Моля въведете желаемата от вас услуга: ");
+        }
 
+        public void ReservateRoom()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----------------Резервиране на стая---------------");
+            Console.WriteLine("Списък със свободни стаи:");
+            DisplayAvaibleRoom();
+            Console.WriteLine("--------------------------------------------------");
+            Console.Write("Моля въведете името на госта, който резервира стаята: ");
+            string guestName = Console.ReadLine();
+            Console.Write("Моля въведете номера на стаята, която искате да резервирате: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Room roomToReserve = data.Rooms.FirstOrDefault(r => r.RoomNumber == roomNumber && !r.Occupied);
+            if (roomToReserve != null)
+            {
+                roomToReserve.Occupied = true;
+                roomToReserve.GuestName = guestName;
+                Console.WriteLine($"Стая номер {roomToReserve.RoomNumber} е успешно резервирана за {guestName}.");
+            }
+            else
+            {
+                Console.WriteLine("Стая с този номер не е намерена или вече е заета.");
+            }
+            data.Save();
+            Console.WriteLine();
+        }
+
+        public void LeaveRoom()
+        {
+            Console.WriteLine();
+            Console.WriteLine("---------------Освобождаване на стая--------------");
+            Console.WriteLine("Списък със заети стаи:");
+            DisplayOccupiedRoom();
+            Console.Write("Моля въведете името на госта, който освобождава стаята: ");
+            string guestName = Console.ReadLine();
+            Room roomToLeave = data.Rooms.FirstOrDefault(r => r.GuestName == guestName && r.Occupied);
+            if (roomToLeave != null)
+            {
+                roomToLeave.Occupied = false;
+                roomToLeave.GuestName = null;
+                Console.WriteLine($"Стая номер {roomToLeave.RoomNumber} е успешно освободена.");
+            }
+            else
+            {
+                Console.WriteLine("Няма намерени стаи за освобождаване с това име.");
+            }
+            data.Save();
+            Console.WriteLine();
+        }
+
+        public void DisplayAvaibleRoomUI()
+        {
+            Console.WriteLine();
+            Console.WriteLine("--------------Списък със свободни стаи------------");
+            DisplayAvaibleRoom();
+            Console.WriteLine();
+        }
+
+        public void DisplayAvaibleRoom()
+        {
+            List<Room> availableRooms = data.DisplayAvaibleRooms();
+            if (availableRooms.Count == 0)
+            {
+                Console.WriteLine("Няма свободни стаи.");
+            }
+            else
+            {
+                foreach (var room in availableRooms)
+                {
+                    Console.WriteLine($"| Стая номер {room.RoomNumber}, Вид: {room.Type}, Цена: {room.PricePerNight} лв. на нощувка");
+                }
+            }
+        }
+        public void DisplayOccupiedRoomUI()
+        {
+            Console.WriteLine();
+            Console.WriteLine("---------------Списък със заети стаи--------------");
+            DisplayOccupiedRoom();
+            Console.WriteLine();
+        }
+
+        public void DisplayOccupiedRoom()
+        {
+            List<Room> occupiedRooms = data.DisplayOccupiedRooms();
+            if (occupiedRooms.Count == 0)
+            {
+                Console.WriteLine("Няма заети стаи.");
+            }
+            else
+            {
+                foreach (var room in occupiedRooms)
+                {
+                    Console.WriteLine($"| Стая номер {room.RoomNumber}, Вид: {room.Type}, Гост: {room.GuestName}");
+                }
+            }
+        }
     }
 }
